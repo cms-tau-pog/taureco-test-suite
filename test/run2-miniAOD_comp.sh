6e3f0bea67bacc0f97c04354786fb8879ed7dc4d
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-02-18 13:07:21 +0100

    add more quantities to run2-miniAOD comparison

--- test/run2-miniAOD_comp.sh
+++ test/run2-miniAOD_comp.sh
@@ -2,9 +2,27 @@
 
 python $TS_DIR/test/compare_miniAOD.py --input-dev $TS_DIR/projects/$TS_PROJECT_NAME/test/run2-miniAOD_dev.root \
     --input-ref $TS_DIR/projects/$TS_PROJECT_NAME/test/run2-miniAOD_ref.root -q \
-    id:byDeepTau2017v2p1VSjetraw \
-    id:byDeepTau2017v2p1VSeraw \
-    id:byDeepTau2017v2p1VSmuraw \
+    pt \
+    eta \
+    phi \
+    mass \
+    mt \
+    decayMode \
     id:chargedIsoPtSum \
     id:neutralIsoPtSum \
-    id:byCombinedIsolationDeltaBetaCorrRaw3Hits
+    id:neutralIsoPtSumWeight \
+    id:footprintCorrection \
+    id:photonPtSumOutsideSignalCone \
+    id:puCorrPtSum \
+    id:chargedIsoPtSumdR03 \
+    id:footprintCorrectiondR03 \
+    id:neutralIsoPtSumWeightdR03 \
+    id:neutralIsoPtSumdR03 \
+    id:photonPtSumOutsideSignalConedR03 \
+    id:byCombinedIsolationDeltaBetaCorrRaw3Hits \
+    id:againstElectronMVA6Raw \
+    id:againstMuonLoose3 \
+    id:againstMuonTight3 \
+    id:byDeepTau2017v2p1VSjetraw \
+    id:byDeepTau2017v2p1VSeraw \
+    id:byDeepTau2017v2p1VSmuraw
