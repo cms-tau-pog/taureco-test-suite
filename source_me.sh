04caf383ab9bc6de3a0df8434e82ad79883dd163
Sebastian Wozniewski <sebastian.wozniewski@kit.edu>
2021-04-30 15:46:29 +0200

    set default release cycle to 12_0_X

--- source_me.sh
+++ source_me.sh
@@ -20,7 +20,7 @@ export TS_PROJECT_NAME=$1
 
 cd $(dirname "${BASH_SOURCE[0]}")
 export TS_DIR=$PWD
-export TS_DEFAULT_RELEASE_CYCLE=11_3_X
+export TS_DEFAULT_RELEASE_CYCLE=12_0_X
 
 #Load functions
 source utils/helper_functions.sh
